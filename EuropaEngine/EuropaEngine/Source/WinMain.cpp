82fc134cb4aebb1abc10add8f4ac9d6a72620779
Ivan <ivansok110503@gmail.com>
2024-09-03 20:22:25 +0300

    Continue

--- EuropaEngine/EuropaEngine/Source/WinMain.cpp
+++ EuropaEngine/EuropaEngine/Source/WinMain.cpp
@@ -2,6 +2,13 @@
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, INT nCmdShow) {
 
+	/* Create Window class */
+
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW
+
 
 	return 0;
 }
